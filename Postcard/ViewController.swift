04a2ccbecb23dad1215c93e833b7845ea265898b
Chris Tan <iChris@Chriss-MacBook-Air.local>
2015-05-26 15:25:36 +0800

    Challenge 1

--- Postcard/ViewController.swift
+++ Postcard/ViewController.swift
@@ -15,6 +15,9 @@ class ViewController: UIViewController {
     @IBOutlet weak var enterMessageTextField: UITextField!
     @IBOutlet weak var mailButton: UIButton!
     
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -32,9 +35,14 @@ class ViewController: UIViewController {
         messageLabel.hidden = false
         messageLabel.text = enterMessageTextField.text
         messageLabel.textColor = UIColor.redColor()
-        
         enterMessageTextField.text = ""
         enterMessageTextField.resignFirstResponder()
+        
+        nameLabel.hidden = false
+        nameLabel.text = enterNameTextField.text
+        nameLabel.textColor = UIColor.blueColor()
+        enterNameTextField.resignFirstResponder()
+        enterNameTextField.text = ""
 
         mailButton.setTitle("Mail Sent", forState: UIControlState.Normal)
         
